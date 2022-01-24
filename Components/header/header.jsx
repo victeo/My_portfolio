0957d1c9078608ac50f5f280dd4a3dc75d5eddc4
victeo <victor.atomo@gmail.com>
2022-01-24 23:43:26 +0000

    menu chavoso

--- Components/header/header.jsx
+++ Components/header/header.jsx
@@ -1,28 +1,34 @@
 import Section1 from "../section1/section1";
 import styles from "./header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+  };
   return (
     <>
+      <header className={styles.headerH}>
+        <div className={active ? styles.header__icon+ ' ' + styles.header__iconActive : styles.header__icon} onClick={ToggleMode}>
+          <div className={styles.header__hamburguer }></div>
+        </div>
+        <div className={active ? styles.header__menu+ ' ' + styles.header__menuOpen : styles.header__menu+ ' ' + styles.header__menuClose }>
+            <ul className={styles.header__ul}>
+                <li className={styles.header__li}>HOME</li>
+                <li className={styles.header__li}>WORK</li>
+                <li className={styles.header__li}>SERVICES</li>
+                <li className={styles.header__li}>CONTACT</li>
+
+            </ul>
+        </div>
+      </header>
+
       <section className={styles.select}>
         <div className={styles.header}>
           <div className={styles.container}>
             <div className={styles.header__logo}>Victor Teodoro</div>
-            <div className={styles.header__menu}>
-              <input
-                type="checkbox"
-                id="checkbox-menu"
-                className={styles.header__menu_input}
-              />
-              <label
-                htmlFor="checkbox-menu"
-                className={styles.header__menu_label}
-              >
-                <span className={styles.header__menu_span}></span>
-                <span className={styles.header__menu_span}></span>
-                <span className={styles.header__menu_span}></span>
-              </label>
-            </div>
+
           </div>
         </div>
         <div className={styles.conteudo}>
---
+++ Model/headerMenu.js
@@ -0,0 +1,16 @@
+import Document from 'next/document'
+
+
+        // const btnHamburguer = document.getElementById('checkbox_menu');
+
+        // function toggleMenu() {
+
+        //     const nav = document.getElementById('header_nav');
+        //     nav.classList.toggle('active');
+
+
+        // }
+
+
+        // btnHamburguer.addEventListener('click', toggleMenu);
+
