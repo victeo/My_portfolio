b50a11c223d6aa14fa5bd5247bd4c2a12b089406
victeo <victor.atomo@gmail.com>
2022-01-29 19:36:40 +0000

    Section work and fixed menu

--- Components/work/work.jsx
+++ Components/work/work.jsx
@@ -1,8 +1,28 @@
-export default function  Work() {
-    return (
-      <>
-        <section id="work"></section>
-      </>
-    );
-  }
-  
+import style from "../work/work.module.css";
+export default function Work() {
+  return (
+    <>
+      <section id="work" className={style.work}>
+        <div className={style.work__work}>
+          <div className={style.work__box__col8}>
+            <h1 className={style.work__title}>HOW ARE I</h1>
+            <h5 className={style.work__frase}>
+              “The simpler explanation is preferable to the more complex one.”
+              <br />
+              <i>Occam's razor </i>
+            </h5>
+            
+            <p className={style.work__text}>
+              I'm a designer that believes that simplicity that is better than
+              complexity. Wherewith creative and hard work is the better way to
+              came in our goal. I believe too that universe conspiracies with
+              us, where the universe law is the action and reaction. So, is
+              important we to be good with each other.
+            </p>
+          </div>
+          <div className={style.work__box__col4}></div>
+        </div>
+      </section>
+    </>
+  );
+}
