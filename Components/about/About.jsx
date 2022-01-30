21ab70b563cb69d9c4962faeeae1265c18643581
victeo <victor.atomo@gmail.com>
2022-01-30 22:52:26 +0000

    Ultimo de hj

---
+++ Components/about/About.jsx
@@ -0,0 +1,11 @@
+import  styles from "../about/about.module.css"
+
+export default function About(){
+    return(
+        <>
+        <section className={styles.about}>
+
+        </section>
+        </>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,19 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Box } from '@chakra-ui/react'
+
 import Header from "../Components/header/header";
 import Section1 from "../Components/section1/section1";
-import Section2 from "../Components/section2/section2";
 import Work from "../Components/work/work";
-
-
+import About from "../Components/about/About";
 
 export default function Home() {
-
   return (
     <>
       <Header />
 
-          <Section1></Section1>
-      
+      <Section1></Section1>
+
       <Work></Work>
 
-
-      
+      <About />
     </>
   );
 }
